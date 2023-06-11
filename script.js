78aa137b67c91deec8fc587479f8e9e54eb3a8a1
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-11 05:55:30 +0300

    Completed assignment find 7

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Good Luck 💪🏾
+
+
+function findSeven(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].toString().includes('7')) {
+        return "Found!";
+      }
+    }
+    return "There is no 7 in the array";
+  }
+
+console.log(findSeven([99, 88, 66, 55, 44])); 
+console.log(findSeven([17, 12, 22, 33, 68, 52])); 
+console.log(findSeven([12, 102, 90, 97, 16, 18]));
+console.log(findSeven([4, 2, 3, 5, 6, 8, 9, 10, 11]));
+console.log(findSeven([100, 200, 207, 300, 400, 10]));
